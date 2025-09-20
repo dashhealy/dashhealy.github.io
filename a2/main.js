4428ea460e2d21fff5f00ec0f484470f80fd0da2
dashhealy <S4129466@student.rmit.edu.au>
2025-09-21 01:18:39 +1000

    more work

--- a2/main.js
+++ a2/main.js
@@ -7,7 +7,15 @@ const playPauseIcon = document.querySelector("#playPauseIcon");
 const timeline = document.querySelector("#timelineProgress");
 const currentTimeText = document.querySelector("#currentTimeFeedback");
 const totalTimeText = document.querySelector("#totalTimeFeedback");
-const helloButton = document.querySelector("#helloButton");
+
+const introButton = document.querySelector("#introButton");
+const step1Button = document.querySelector("#step1Button");
+const step2Button = document.querySelector("#step2Button");
+const step3Button = document.querySelector("#step3Button");
+const step4Button = document.querySelector("#step4Button");
+const step5Button = document.querySelector("#step5Button");
+const step6Button = document.querySelector("#step6Button");
+const step7Button = document.querySelector("#step7Button");
 
 /* when js loads remove defaul controls */
 videoElement.removeAttribute("controls");
@@ -90,8 +98,145 @@ function jumpToTime(ev) {
   videoElement.currentTime = videoElement.duration * clickPercent;
 }
 
-helloButton.addEventListener("click", changeTime);
+introButton.addEventListener("click", introTime);
 
-function changeTime() {
+function introTime() {
+  videoElement.currentTime = 2;
+}
+
+step1Button.addEventListener("click", step1Time);
+
+function step1Time() {
+  videoElement.currentTime = 28;
+}
+
+step2Button.addEventListener("click", step2Time);
+
+function step2Time() {
+  videoElement.currentTime = 40;
+}
+
+step3Button.addEventListener("click", step3Time);
+
+function step3Time() {
+  videoElement.currentTime = 71;
+}
+
+step4Button.addEventListener("click", step4Time);
+
+function step4Time() {
+  videoElement.currentTime = 127;
+}
+
+step5Button.addEventListener("click", step5Time);
+
+function step5Time() {
+  videoElement.currentTime = 155;
+}
+
+step6Button.addEventListener("click", step6Time);
+
+function step6Time() {
   videoElement.currentTime = 200;
 }
+
+step7Button.addEventListener("click", step7Time);
+
+function step7Time() {
+  videoElement.currentTime = 240;
+}
+
+videoElement.addEventListener("timeupdate", changeColour);
+
+function changeColour() {
+  let currentSeconds = Math.floor(videoElement.currentTime);
+  parseInt(currentSeconds);
+  console.log(currentSeconds);
+  if (currentSeconds >= 2 && currentSeconds <= 27) {
+    introButton.classList.add("highlight");
+    step1Button.classList.remove("highlight");
+    step2Button.classList.remove("highlight");
+    step3Button.classList.remove("highlight");
+    step4Button.classList.remove("highlight");
+    step5Button.classList.remove("highlight");
+    step6Button.classList.remove("highlight");
+    step7Button.classList.remove("highlight");
+  } else if (currentSeconds >= 28 && currentSeconds <= 39) {
+    introButton.classList.remove("highlight");
+    step1Button.classList.add("highlight");
+    step2Button.classList.remove("highlight");
+    step3Button.classList.remove("highlight");
+    step4Button.classList.remove("highlight");
+    step5Button.classList.remove("highlight");
+    step6Button.classList.remove("highlight");
+    step7Button.classList.remove("highlight");
+  } else if (currentSeconds >= 40 && currentSeconds <= 70) {
+    introButton.classList.remove("highlight");
+    step1Button.classList.remove("highlight");
+    step2Button.classList.add("highlight");
+    step3Button.classList.remove("highlight");
+    step4Button.classList.remove("highlight");
+    step5Button.classList.remove("highlight");
+    step6Button.classList.remove("highlight");
+    step7Button.classList.remove("highlight");
+  } else if (currentSeconds >= 71 && currentSeconds <= 126) {
+    introButton.classList.remove("highlight");
+    step1Button.classList.remove("highlight");
+    step2Button.classList.remove("highlight");
+    step3Button.classList.add("highlight");
+    step4Button.classList.remove("highlight");
+    step5Button.classList.remove("highlight");
+    step6Button.classList.remove("highlight");
+    step7Button.classList.remove("highlight");
+  } else if (currentSeconds >= 127 && currentSeconds <= 154) {
+    introButton.classList.remove("highlight");
+    step1Button.classList.remove("highlight");
+    step2Button.classList.remove("highlight");
+    step3Button.classList.remove("highlight");
+    step4Button.classList.add("highlight");
+    step5Button.classList.remove("highlight");
+    step6Button.classList.remove("highlight");
+    step7Button.classList.remove("highlight");
+  } else if (currentSeconds >= 155 && currentSeconds <= 199) {
+    introButton.classList.remove("highlight");
+    step1Button.classList.remove("highlight");
+    step2Button.classList.remove("highlight");
+    step3Button.classList.remove("highlight");
+    step4Button.classList.remove("highlight");
+    step5Button.classList.add("highlight");
+    step6Button.classList.remove("highlight");
+    step7Button.classList.remove("highlight");
+  } else if (currentSeconds >= 200 && currentSeconds <= 239) {
+    introButton.classList.remove("highlight");
+    step1Button.classList.remove("highlight");
+    step2Button.classList.remove("highlight");
+    step3Button.classList.remove("highlight");
+    step4Button.classList.remove("highlight");
+    step5Button.classList.remove("highlight");
+    step6Button.classList.add("highlight");
+    step7Button.classList.remove("highlight");
+  } else if (currentSeconds >= 240) {
+    introButton.classList.remove("highlight");
+    step1Button.classList.remove("highlight");
+    step2Button.classList.remove("highlight");
+    step3Button.classList.remove("highlight");
+    step4Button.classList.remove("highlight");
+    step5Button.classList.remove("highlight");
+    step6Button.classList.remove("highlight");
+    step7Button.classList.add("highlight");
+  } else {
+    introButton.classList.remove("highlight");
+    step1Button.classList.remove("highlight");
+    step2Button.classList.remove("highlight");
+    step3Button.classList.remove("highlight");
+    step4Button.classList.remove("highlight");
+    step5Button.classList.remove("highlight");
+    step6Button.classList.remove("highlight");
+    step7Button.classList.remove("highlight");
+  }
+}
+
+// classList.add() adds class
+// classList.remove() removes class
+// classList.toggle() toggles class
+// introButton.classList.remove("highlight");
