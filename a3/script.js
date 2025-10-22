15d29374a90e69cf2d82ae8ab22ab52dda739223
dashhealy <S4129466@student.rmit.edu.au>
2025-10-22 21:10:50 +1100

    added text

--- a3/script.js
+++ a3/script.js
@@ -1,27 +1,37 @@
 const starryNightSmall = document.querySelector(".starryNightSmall");
 const starryNightBig = document.querySelector("#starryNightBig");
+const starryNightText = document.querySelector(".starryNightText");
 
 const birthOfVenusSmall = document.querySelector(".birthOfVenusSmall");
 const birthOfVenusBig = document.querySelector("#birthOfVenusBig");
+const birthOfVenusText = document.querySelector(".birthOfVenusText");
 
 starryNightSmall.addEventListener("mouseover", function () {
   starryNightBig.classList.remove("animateOut");
   starryNightBig.classList.add("animateIn");
+  starryNightText.classList.remove("animateOut");
+  starryNightText.classList.add("animateIn");
 });
 
 starryNightSmall.addEventListener("mouseleave", function () {
-  starryNightBig.classList.remove("animateIn");
   starryNightBig.classList.add("animateOut");
+  starryNightBig.classList.remove("animateIn");
+  starryNightText.classList.add("animateOut");
+  starryNightText.classList.remove("animateIn");
 });
 
 birthOfVenusSmall.addEventListener("mouseover", function () {
   birthOfVenusBig.classList.remove("animateOut");
   birthOfVenusBig.classList.add("animateIn");
+  birthOfVenusText.classList.remove("animateOut");
+  birthOfVenusText.classList.add("animateIn");
 });
 
 birthOfVenusSmall.addEventListener("mouseleave", function () {
   birthOfVenusBig.classList.add("animateOut");
   birthOfVenusBig.classList.remove("animateIn");
+  birthOfVenusText.classList.add("animateOut");
+  birthOfVenusText.classList.remove("animateIn");
 });
 
 // function starryAnimIn() {
